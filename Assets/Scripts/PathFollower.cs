6a9181841464c1f690a87fe94d15b380b85ce46a
BNS-MarkUlrich <31822@ma-web.nl>
2020-09-11 14:43:44 +0200

    Added first working waypoint

--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Transform = UnityEngine.Transform;
 
 namespace Opdrachten
 {
@@ -11,6 +12,15 @@ namespace Opdrachten
     /// </summary>
     public class PathFollower : MonoBehaviour
     {
+        public Transform target;
+        public float speed = 1.0f;
+        //public Waypoint position;
 
+        void Update()
+        {
+            transform.LookAt(target);
+            float step = speed * Time.deltaTime;
+            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+        }
     }
 }
