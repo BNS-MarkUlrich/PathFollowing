1edf8751c1b7b9dcf1b276b2a51dfed23ece4316
BNS-MarkUlrich <31822@ma-web.nl>
2020-09-15 10:25:36 +0200

    stable version, not using 3 classes

--- Assets/Scripts/Path.cs
+++ Assets/Scripts/Path.cs
@@ -13,37 +13,12 @@ namespace Opdrachten
     /// </summary>
     public class Path : MonoBehaviour
     {
-        public GameObject[] waypoints;
-
-        /// <summary>
-        /// De onderstaande functie is een template gehaald van het internet, 
-        /// deze werkt compleet en behaald het doel van de opdracht, 
-        /// helaas moet ik voor de opracht met alle 3 de classes werken.
-        /// </summary>
-        public int current = 0;
-        float rotSpeed;
-        public float speed = 1.0f;
-        float WPradius = 1;
-        void Update()
-        {
-            if (Vector3.Distance(waypoints[current].transform.position, transform.position) < WPradius)
-            {
-                current++;
-                if (current >= waypoints.Length)
-                {
-                    current = 0;
-                }
-            }
-            transform.position = Vector3.MoveTowards(transform.position, waypoints[current].transform.position, Time.deltaTime * speed);
-
-        }
 
         /// <summary>
         /// Deze functie returned het volgende waypoint waar naartoe kan worden bewogen.
         /// </summary>
         public Waypoint GetNextWaypoint()
         {
-            
             // dit is nu niet goed, zorg ervoor dat het goede waypoint wordt teruggegeven.
             return null;
         }
--- Assets/Scripts/PathFollower.cs
+++ Assets/Scripts/PathFollower.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using UnityEditorInternal;
 using UnityEngine;
 //using Transform = UnityEngine.Transform;
 
@@ -10,16 +11,64 @@ namespace Opdrachten
     /// Deze class zorgt ervoor dat het object (in Tower Defense) vaak een enemy, het path afloopt
     /// tip: je kunt de transform.LookAt() functie gebruiken en vooruitbewegen.
     /// </summary>
+
     public class PathFollower : MonoBehaviour
     {
-        public Transform target;
-        public float speed = 1.0f;
+        [SerializeField] private Transform[] _waypoints;
 
-        void Update()
+        [SerializeField] private float _speed;
+        [SerializeField] private float _arrivalthreshold = 0.1f;
+
+        private int _currentWaypointIndex;
+
+        private void Start()
         {
-            transform.LookAt(target);
-            float step = speed * Time.deltaTime;
-            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+            _currentWaypointIndex = 0;
+        }
+
+        private void Update()
+        {
+            Vector3 heightOffsetPosition = new Vector3(_waypoints[_currentWaypointIndex].position.x,
+                transform.position.y, _waypoints[_currentWaypointIndex].position.z);
+            float distance = Vector3.Distance(transform.position, heightOffsetPosition);
+
+            if (distance <= _arrivalthreshold)
+            {
+                if (_currentWaypointIndex == _waypoints.Length - 1)
+                {
+                    print("Ik ben bij het eindpunt");
+                    
+                }
+                else
+                {
+                    _currentWaypointIndex++;
+                }
+            }
+            else
+            {
+                transform.LookAt(heightOffsetPosition);
+                transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+            }
         }
     }
 }
+
+
+
+    /// <summary>
+    /// Below class is old version
+    /// </summary>
+
+//    public class PathFollower : MonoBehaviour
+//    {
+//        public Transform target;
+//        public float speed = 1.0f;
+
+//        void Update()
+//        {
+//            transform.LookAt(target);
+//            float step = speed * Time.deltaTime;
+//            transform.position = Vector3.MoveTowards(transform.position, target.position, step);
+//        }
+//    }
+//}
