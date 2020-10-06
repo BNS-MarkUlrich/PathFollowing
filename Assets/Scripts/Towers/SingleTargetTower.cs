8bbdd8204d424509e36c577baad890f4d21c5d64
BNS-MarkUlrich <31822@ma-web.nl>
2020-10-06 17:51:31 +0200

    SingleTargetTower now has working damage system, still needs code cleaning and optimisation

--- Assets/Scripts/Towers/SingleTargetTower.cs
+++ Assets/Scripts/Towers/SingleTargetTower.cs
@@ -8,26 +8,47 @@ namespace Opdrachten
     public class SingleTargetTower : Tower // Rename to "(Heavy) Cannon"
     {
         private Enemy _target;
+
         protected override bool CanAttack()
         {
             _target = _rangeChecker.GetFirstEnemyInRange();
             return _target != null;
         }
-
+        
         protected override void Attack()
         {
             //onAttacked.Invoke();
             if (CanAttack())
-            {                
-                StartCoroutine(ShootTimer());
+            {
+                //Debug.Log(CanAttack() + "While");
+                //_isShooting = true;
+                if (_isShooting != true)
+                {
+                    StartCoroutine(ShootTimer());
+                    _enemyHealth.EnemyTakeDamage(_damageAmount);
+                }
             }
+            //for (int i = 0; i < _enemiesHealth.Length-1; i++)
+            //{
+            //    if (CanAttack())
+            //    {
+            //        StartCoroutine(ShootTimer());
+            //    }
+            //}
             //Debug.Log("SingleTargetTower: Ik heb 1 target en val deze aan!");
         }
 
         IEnumerator ShootTimer()
         {
-            _enemyHealth.EnemyTakeDamage(_damageAmount);
-            yield return new WaitForSeconds(1f);
+            _isShooting = true;
+            while (_isShooting == true)
+            {
+                yield return new WaitForSeconds(1f);
+                _isShooting = false;
+            }
+            //_enemyHealth.EnemyTakeDamage(_damageAmount);
+            yield return !CanAttack();
+            Debug.Log(CanAttack() + "ShootTmer");
             Attack();
         }
     }
--- Assets/Scripts/Towers/Tower.cs
+++ Assets/Scripts/Towers/Tower.cs
@@ -11,17 +11,33 @@ namespace Opdrachten
     {
         protected EnemyInRangeChecker _rangeChecker;
         protected EnemyHealth _enemyHealth;
+        protected EnemyHealth[] _enemiesHealth;
+        protected bool _isShooting = false;
         //protected UnityEvent onAttacked;
         [SerializeField] protected float _damageAmount;
+
+
+        private float duration = 4.0f;
+        private float timer = 0.0f;
 
         private void Awake()
         {
             _rangeChecker = GetComponent<EnemyInRangeChecker>();
             _enemyHealth = GameObject.FindWithTag("Enemy").GetComponent<EnemyHealth>();
+            //_enemiesHealth = GameObject.FindObjectsOfType<EnemyHealth>();
+            //Debug.Log(_enemiesHealth);
         }
 
         void Update() //FixedUpdate
         {
+            timer += Time.deltaTime;
+            if(timer > duration)
+            {
+                Attack();
+                timer = 0;
+            }
+
+
             // als we niet kunnen aanvallen. Ga dan uit de update functie
             if (!CanAttack()) return;
 
